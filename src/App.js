8a4f3397b257e4a023399a4642ddc27cef646737
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2020-03-08 17:23:38 +0700

    belajar awal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className="showcase">
+			<div className="container">
+				<nav>
+					<h1 className="logo">My Website</h1>
+					<ul>
+						<li><a href="#home">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#service">Services</a></li>
+					</ul>
+				</nav>
+
+				<div className="showcase-content">
+					<div>
+						<h1>Make Your Marketing Real</h1>
+						<p className="my-1">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+							eligendi tempore atque laborum. Quisquam nemo at non. Corrupti,
+							vitae dolore.
+						</p>
+						<a href="#learn" className="btn-primary">Learn More</a>
+						<a href="#sign" className="btn-secondary">Sign Up</a>
+					</div>
+					<img src="https://themesbrand.com/zooki/layouts/images/home-2-img.png" alt="" />
+				</div>
+			</div>
+		</header>
     </div>
   );
 }
